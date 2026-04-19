1be7db4418e26cea5fac697b4257240f5d067644
chanCo <loinsective@naver.com>
2026-04-20 04:07:17 +0900

    ADD: Card 컴포넌트

---
+++ src/shared/components/ui/ Card.tsx
@@ -0,0 +1,73 @@
+/**
+ * @file: Card.tsx
+ * @author: chad
+ * @since: 2026.04.20 ~
+ * @description: Card 컴포넌트
+ */
+
+'use client';
+
+import React from 'react';
+import { cn } from '@/shared/lib/utils';
+import { cva, VariantProps } from 'class-variance-authority';
+
+const cardVariants = cva('rounded-lg cursor-pointer', {
+  variants: {
+    variant: {
+      default: 'bg-gray-1',
+      outline: 'border border-border-primary',
+      shadowed: 'shadow-md',
+      white: 'bg-white',
+    },
+    size: {
+      md: 'px-3 py-3',
+      sm: 'px-2 py-2',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+    size: 'md',
+  },
+});
+
+interface ICard
+  extends
+    React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof cardVariants> {
+  className?: string;
+  disabled?: boolean;
+}
+
+function CardEntity(
+  { className, disabled, variant, size, ...props }: ICard,
+  ref: React.ForwardedRef<HTMLDivElement>,
+) {
+  const [isSelected, setIsSelected] = React.useState(false);
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (disabled) return;
+
+    if (props.onClick) {
+      props.onClick(e);
+    }
+
+    setIsSelected(!isSelected);
+  };
+
+  return (
+    <div
+      className={cn(
+        cardVariants({ variant, size }),
+        disabled && 'pointer-events-none opacity-50',
+        isSelected && 'outline outline-border-active',
+        className,
+      )}
+      ref={ref}
+      onClick={handleClick}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export const Card = React.forwardRef(CardEntity);
