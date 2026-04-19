4e08a4a6ac0cc3ab7a9e7813bc3c9e5ebc81d367
chanCo <loinsective@naver.com>
2026-04-19 13:40:28 +0900

    ADD: tailwind v4에 맞는 theme 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import '@/shared/styles/globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: '매필로그: Mafilog',
@@ -24,10 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      lang="ko"
     >
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex flex-col">{children}</body>
     </html>
   );
 }
