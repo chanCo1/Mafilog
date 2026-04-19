7b4c984924ffd08af32280d0853c5fc4d9d79b04
chanCo <loinsective@naver.com>
2026-04-20 00:50:20 +0900

    ADD: 필수 입력임을 보여주는 RequireDot 컴포넌트

---
+++ src/shared/components/ui/RequireDot.tsx
@@ -0,0 +1,12 @@
+/**
+ * @file: RequireDot.tsx
+ * @author: chad
+ * @since: 2026.04.20 ~
+ * @description: RequireDot 컴포넌트 (필수 입력일 때 사용)
+ */
+
+export default function RequireDot() {
+  return (
+    <div className='w-1 h-1 bg-state-error rounded-full' />
+  );
+}
