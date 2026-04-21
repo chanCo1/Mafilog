a861cb78553a4f43708a3f35ce2c2c2903fb805c
chanCo <loinsective@naver.com>
2026-04-21 14:51:56 +0900

    ADD: 서비스 간략 소개 탭 리스트 constant

---
+++ src/features/home/constants/index.ts
@@ -0,0 +1,11 @@
+import { ILabelValue } from '@/shared/interfaces';
+
+/** 서비스 간략 소개 탭 리스트 */
+export const TABLIST: ILabelValue[] = [
+  {label: '일정', value: 'schedule'},
+  {label: '가계부', value: 'household'},
+  {label: '추억', value: 'memory'},
+  {label: '타임라인', value: 'timeline'}
+]
+/** TABLIST 타입 추출 */
+export type TABLIST = (typeof TABLIST)[keyof typeof TABLIST];
