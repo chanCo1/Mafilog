e2867196e906ba2b8ac3aab3f4d2a538a322f2e0
chanCo <loinsective@naver.com>
2026-04-21 15:21:52 +0900

    UPD: ILabelValue에 제네릭 추가

--- src/features/home/constants/index.ts
+++ src/features/home/constants/index.ts
@@ -1,7 +1,8 @@
 import { ILabelValue } from '@/shared/interfaces';
+import { TTabTypes } from '@/features/home/types';
 
 /** 서비스 간략 소개 탭 리스트 */
-export const TABLIST: ILabelValue[] = [
+export const TABLIST: ILabelValue<TTabTypes>[] = [
   {label: '일정', value: 'schedule'},
   {label: '가계부', value: 'household'},
   {label: '추억', value: 'memory'},
