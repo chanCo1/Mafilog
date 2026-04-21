877910c3e912ea89e9e555e2ecaec4bac7fb9d7b
chanCo <loinsective@naver.com>
2026-04-21 16:26:12 +0900

    ADD: 탭별 간략 소개 리스트 추가

--- src/features/home/constants/index.ts
+++ src/features/home/constants/index.ts
@@ -10,3 +10,35 @@ export const TABLIST: ILabelValue<TTabTypes>[] = [
 ]
 /** TABLIST 타입 추출 */
 export type TABLIST = (typeof TABLIST)[keyof typeof TABLIST];
+
+/** 일정 소개 리스트 */
+export const SCHADULE_INTRODUCE_LIST: string[] = [
+  '지구 곳곳에 있는 장소 검색',
+  '여행 기간에 맞는 일정 추가/변경',
+  '여행 스타일에 맞는 체크리스트',
+  '환율 · 날씨 등 현지 정보 확인',
+]
+
+/** 가계부 소개 리스트 */
+export const HOUSEHOLD_INTRODUCE_LIST: string[] = [
+  '상황에 맞는 지출 내역 기록',
+  '환율 자동 계산으로 편안하게 사용 내역 확인',
+  '한 눈에 보이는 통계',
+  '지출에 따라 정산 내역 자동 계산',
+  '매일 업데이트 되는 현지 환율 정보',
+]
+
+/** 추억 채우기 소개 리스트 */
+export const MEMORY_INTRODUCE_LIST: string[] = [
+  '국내 지도와 해외 지도 어디든 원하는 곳에 기록',
+  '지도에 원하는 사진 업로드',
+  '다녀온 여행 일정 연동으로 정확한 추억 회상',
+]
+
+/** 타임라인 소개 리스트 */
+export const TIMELINE_INTRODUCE_LIST: string[] = [
+  '국 · 내외 / 연도별 타임라인 필터링',
+  '지금까지 여행에 대한 통계',
+  '시간대별 여행 정보 확인',
+  '각 여행에 대해 카테고리별 지출 내역',
+]
