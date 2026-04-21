df79fa567adbc8aa8a3df51908f8989bb7ef2f25
chanCo <loinsective@naver.com>
2026-04-21 17:35:21 +0900

    ADD:HomePage 컴포넌트

---
+++ src/features/home/pages/HomePage.tsx
@@ -0,0 +1,72 @@
+/**
+ * @file: HomePage.tsx
+ * @author: chad
+ * @since: 2026.04.21 ~
+ * @description: HomePage 컴포넌트
+ */
+
+import React from 'react';
+import { cn } from '@/shared/lib/utils';
+import { DEFAULT_LAYOUT_CLASSNAME } from '@/shared/constants';
+import { LogoText } from '@/shared/components/ui/LogoText';
+import LogoDescription from '@/features/home/components/LogoDescription';
+import { Button } from '@/shared/components/ui/Button';
+import BriefIntroduceTabs from '@/features/home/components/briefTab/BriefIntroduceTabs';
+
+export default function HomePage() {
+  return (
+    <>
+      <div className="h-auto">
+        <div className="bg-gray-6 relative left-1/2 -mt-7 h-87.5 w-screen -translate-x-1/2">
+          <div className={cn(DEFAULT_LAYOUT_CLASSNAME, 'px-3')}>
+            {/* 로고 설명 */}
+            <div className='flex flex-col gap-7.5 items-center'>
+              <div className="flex flex-col gap-3 pt-15 items-center">
+                <div className="flex flex-col items-center">
+                  <p className="text-lg text-white">
+                    지도에 색칠하듯 기록하기 쉬운 여행
+                  </p>
+                  <LogoText size="logo" color="white" />
+                  <div className="flex items-center gap-1">
+                    <LogoDescription title="Map." subTitle="지도를" />
+                    <LogoDescription title="Fill." subTitle="채우고" />
+                    <LogoDescription title="Log." subTitle="기록하다" />
+                  </div>
+                </div>
+                <div className="flex gap-2.5">
+                  <Button size="lg">내 여행</Button>
+                  <Button variant="secondary" size="lg">
+                    추억 채우기
+                  </Button>
+                </div>
+              </div>
+              {/* 서비스 간략 소개 */}
+              <div className='w-175'>
+                <BriefIntroduceTabs />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pt-60">
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+        <div>asdasdas</div>
+      </div>
+    </>
+  );
+}
