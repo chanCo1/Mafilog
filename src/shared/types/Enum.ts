41e11e3fc66c5a4c50fcbf25684c774f1d480277
chanCo <loinsective@naver.com>
2026-04-21 20:14:09 +0900

    ADD: 가계부 지출 타입, 여행 타입 enum

--- src/shared/types/Enum.ts
+++ src/shared/types/Enum.ts
@@ -1,6 +1,34 @@
 /** 일정 타입 */
 export const SCHEDULE_TYPE = {
+  /** 장소 */
   LOCATION: 'location',
+  /** 메모 */
   MEMO: 'memo',
 };
 export type SCHEDULE_TYPE = (typeof SCHEDULE_TYPE)[keyof typeof SCHEDULE_TYPE];
+
+/** 가계부 지출 타입 */
+export const EXPENSES_TYPE = {
+  /** 교통 */
+  BUS: 'bus',
+  /** 음식 */
+  FOOD: 'food',
+  /** 쇼핑 */
+  SHOPPING: 'shopping',
+  /** 관광 */
+  TOUR: 'tour',
+  /** 숙소 */
+  HOUSE: 'house',
+  /** 기타 */
+  ETC: 'etc',
+};
+export type EXPENSES_TYPE = (typeof EXPENSES_TYPE)[keyof typeof EXPENSES_TYPE];
+
+/** 여행 타입 */
+export const TRAVEL_TYPE = {
+  /** 해외 */
+  INTERNATIONAL: 'international',
+  /** 국내 */
+  LOCAL: 'local',
+};
+export type TRAVEL_TYPE = (typeof TRAVEL_TYPE)[keyof typeof TRAVEL_TYPE];
