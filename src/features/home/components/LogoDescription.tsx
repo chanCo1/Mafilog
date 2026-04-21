dd16ad87fcd1fbd0c23211e08529c7ffb099afff
chanCo <loinsective@naver.com>
2026-04-21 13:59:14 +0900

    ADD: 로고에 대한 부가 설명 컴포넌틑

---
+++ src/features/home/components/LogoDescription.tsx
@@ -0,0 +1,23 @@
+/**
+ * @file: LogoDescription.tsx
+ * @author: chad
+ * @since: 2026.04.21 ~
+ * @description: LogoDescription 컴포넌트 - 로고에 대한 부가설명
+ */
+
+import React from 'react';
+import { cn } from '@/shared/lib/utils';
+
+interface ILogoDescription {
+  title: string;
+  subTitle: string;
+}
+
+export default function LogoDescription({ title, subTitle }: ILogoDescription) {
+  return (
+    <div className="flex flex-col">
+      <span className="text-text-caption">{title}</span>
+      <span className="text-text-secondary text-xs">{subTitle}</span>
+    </div>
+  );
+}
