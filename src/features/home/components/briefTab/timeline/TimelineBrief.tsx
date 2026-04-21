bbac501f64ed13ea1d8590d12a92816b0950ffa0
chanCo <loinsective@naver.com>
2026-04-22 03:35:57 +0900

    ADD: 타임라인 간략 소개

--- src/features/home/components/briefTab/timeline/TimelineBrief.tsx
+++ src/features/home/components/briefTab/timeline/TimelineBrief.tsx
@@ -7,14 +7,62 @@
 
 import { memo } from 'react';
 import { cn } from '@/shared/lib/utils';
+import { Card } from '@/shared/components/ui/Card';
+import Image from 'next/image';
+import { IntroduceList } from '@/features/home/components/briefTab/IntroduceList';
+import {
+  TIMELINE_INTRODUCE_LIST,
+  TIMELINE_MOCK_DATA,
+} from '@/features/home/constants';
+import TimelineCard from '@/features/home/components/briefTab/timeline/TimelineCard';
 
-interface ITimelineBrief {}
+// interface ITimelineBrief {}
 
 function TimelineBrief() {
   return (
-    <div>
-      <div>TimelineBrief</div>
-    </div>
+    <Card className="flex h-full gap-2.5" readonly>
+      {/* 왼쪽 설명 */}
+      <div className="max-mobile:w-full flex w-2/5 flex-col gap-4 rounded-md bg-white p-4">
+        <div>
+          <p className="text-xl font-bold">
+            언제·어디서·얼마나 <br /> 지출했는지 한 눈에
+          </p>
+          <p className="text-text-secondary">
+            그동안 다녔던 여행을 타임라인으로 보고, 지출내역도 확인해보세요
+          </p>
+        </div>
+        <div className="flex flex-col gap-1.5">
+          {TIMELINE_INTRODUCE_LIST.map((list, index) => (
+            <IntroduceList key={index} list={list} />
+          ))}
+        </div>
+      </div>
+
+      {/* 오른쪽 이미지 */}
+      <div className="max-mobile:hidden relative w-3/5 p-4">
+        <div className="w-63">
+          <Image
+            src="/timeline_chart.png"
+            alt="세계 지도 이미지"
+            width={300}
+            height={1}
+            className="object-contain opacity-10"
+          />
+        </div>
+        <div className="absolute top-1/2 right-0 flex w-3/5 -translate-y-1/2 flex-col">
+          {TIMELINE_MOCK_DATA.map((data, index) => (
+            <TimelineCard
+              key={index}
+              name={data.name}
+              index={TIMELINE_MOCK_DATA.length - index}
+              partner={data.partner}
+              style={data.style}
+              type={data.type}
+            />
+          ))}
+        </div>
+      </div>
+    </Card>
   );
 }
 
---
+++ src/features/home/components/briefTab/timeline/TimelineCard.tsx
@@ -0,0 +1,78 @@
+/**
+ * @file: TimelineCard.tsx
+ * @author: chad
+ * @since: 2026.04.22 ~
+ * @description: TimelineCard 컴포넌트
+ */
+
+import { useMemo } from 'react';
+import { cn } from '@/shared/lib/utils';
+import { Card } from '@/shared/components/ui/Card';
+import { CategoryIcon } from '@/shared/components/ui/CategoryIcon';
+import { Chip } from '@/shared/components/ui/Chip';
+import { TRAVEL_TYPE, ICON_TYPE } from '@/shared/types/Enum';
+import { TIconList } from '@/shared/types';
+
+interface ITimelineCard {
+  name: string;
+  type: TRAVEL_TYPE;
+  index: string | number;
+  style: string[];
+  partner: string;
+}
+
+export default function TimelineCard({
+  name,
+  index,
+  partner,
+  style,
+  type,
+}: ITimelineCard) {
+  const getTravelType = useMemo(() => {
+    switch (type) {
+      case TRAVEL_TYPE.INTERNATIONAL:
+        return ICON_TYPE.PLANE;
+      case TRAVEL_TYPE.INTERNATIONAL:
+        return ICON_TYPE.BUS;
+      default:
+        return ICON_TYPE.BUS;
+    }
+  }, [type]);
+
+  return (
+    <div className="flex w-full gap-3">
+      <div className="flex flex-col items-center">
+        <div className="shrink-0">
+          <CategoryIcon variant={getTravelType as TIconList} />
+        </div>
+        <div
+          className={cn(
+            'border-primary w-px flex-1 border',
+            index === 1 && 'border-dashed',
+          )}
+        />
+      </div>
+      <div className="w-full pb-2.5">
+        <Card
+          variant="shadowed"
+          className="flex flex-col items-start justify-center"
+        >
+          <div className="flex items-center gap-1">
+            <span className="text-text-secondary">#{index}</span>
+            <p className="text-lg font-bold">{name}</p>
+          </div>
+          <div className="flex gap-1">
+            <Chip variant="gray" size="sm">
+              #{partner}
+            </Chip>
+            {style.map((_style) => (
+              <Chip key={_style} variant="gray" size="sm">
+                #{_style}
+              </Chip>
+            ))}
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
+}
