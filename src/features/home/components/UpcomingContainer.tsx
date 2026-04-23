dd36d2cd7eae06ca1327b2145cce3c042705ff8f
chanCo <loinsective@naver.com>
2026-04-23 21:20:29 +0900

    ADD: 다가오는 여행 노출 부분 컴포넌트

---
+++ src/features/home/components/UpcomingContainer.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+/**
+ * @file: UpcomingContainer.tsx
+ * @author: chad
+ * @since: 2026.04.23 ~
+ * @description: UpcomingContainer 컴포넌트, 다가오는 여행 노출
+ */
+
+import { useState } from 'react';
+import { cn } from '@/shared/lib/utils';
+import { Button } from '@/shared/components/ui/Button';
+import { useRouter } from 'next/navigation';
+import { useAuthManagerStore } from '@/shared/stores/useAuthManagerStore';
+
+interface IUpcomingContainer {}
+
+export default function UpcomingContainer() {
+  const router = useRouter();
+  const { isLoggedIn } = useAuthManagerStore();
+
+  return (
+    <>
+      {isLoggedIn ? (
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center gap-2">
+            <p className="text-xl font-bold">다가오는 여행</p>
+            <Button size="xs" onClick={() => router.push('/')}>
+              여행 만들기
+            </Button>
+          </div>
+          <p className="text-text-secondary">
+            다가오는 여행이 없어요! 다음 여행지는 어디인가요?
+          </p>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center gap-2.5">
+          <span className="max-mobile:text-lg text-center text-lg font-bold break-keep">
+            매필로그와 함께 일정을 만들고 즐거웠던 여행을 기록해보세요
+          </span>
+          <Button
+            variant="primaryOutline"
+            size="sm"
+            onClick={() => router.push('/login')}
+          >
+            로그인
+          </Button>
+        </div>
+      )}
+    </>
+  );
+}
