ea8b8644b9e76cb5a97cdfa67674930b1d7d4521
chanCo <loinsective@naver.com>
2026-04-23 21:55:01 +0900

    ADD: 페이지 헤더 타이틀 컴포넌트

---
+++ src/shared/components/ui/PageHeader.tsx
@@ -0,0 +1,35 @@
+/**
+ * @file: PageHeader.tsx
+ * @author: chad
+ * @since: 2026.04.23 ~
+ * @description: PageHeader 컴포넌트
+ */
+
+import { ReactNode } from 'react';
+
+interface IPageHeader {
+  ruby?: ReactNode;
+  title: string;
+  description?: string;
+  titleBtn?: ReactNode;
+}
+
+export default function PageHeader({
+  title,
+  description,
+  titleBtn,
+  ruby,
+}: IPageHeader) {
+  return (
+    <div className="flex flex-col">
+      {ruby}
+      <div className="flex gap-1">
+        <h3 className="text-h3 font-bold">{title}</h3>
+        {titleBtn}
+      </div>
+      {description && (
+        <span className="text-text-secondary text-lg">{description}</span>
+      )}
+    </div>
+  );
+}
