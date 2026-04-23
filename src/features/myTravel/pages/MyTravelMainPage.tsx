068755d2890902a8bd13c482761cd80c4a423966
chanCo <loinsective@naver.com>
2026-04-24 00:59:10 +0900

    UPD: 사이드모달 연결 테스트

--- src/features/myTravel/pages/MyTravelMainPage.tsx
+++ src/features/myTravel/pages/MyTravelMainPage.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 /**
  * @file: MainPage.tsx
  * @author: chad
@@ -8,17 +10,38 @@
 import { useState } from 'react';
 import PageHeader from '@/shared/components/ui/PageHeader';
 import { Button } from '@/shared/components/ui/Button';
+import { SideModal } from '@/shared/components/ui/SideModal';
 
 interface IMyTravelMainPage {}
 
 export default function MyTravelMainPage() {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
   return (
-    <div className='flex flex-col gap-7'>
-      <PageHeader title='내 여행' description='내 모든 여행을 한 곳에서 확인해보세요' />
-      <div className='flex flex-col gap-1 items-center pt-20'>
-        <p className='text-lg text-text-secondary'>아직 다녀온 여행이 없어요. 첫 번째 여행지는 어디인가요?</p>
-        <Button variant='secondary' size='lg'>새 여행 만들기</Button>
+    <>
+      <div className="flex flex-col gap-7">
+        <PageHeader
+          title="내 여행"
+          description="내 모든 여행을 한 곳에서 확인해보세요"
+        />
+        <div className="flex flex-col items-center gap-1 pt-20">
+          <p className="text-text-secondary text-center text-lg break-keep">
+            아직 다녀온 여행이 없어요. 첫 번째 여행지는 어디인가요?
+          </p>
+          <Button variant="secondary" size="lg" onClick={() => setIsOpenModal(true)}>
+            새 여행 만들기
+          </Button>
+        </div>
       </div>
-    </div>
+      <SideModal
+        isOpen={isOpenModal}
+        title='새 여행 만들기'
+        handleClose={() => setIsOpenModal(false)}
+        okLabel="다음"
+        cancelLabel="닫기"
+      >
+        asd
+      </SideModal>
+    </>
   );
 }
