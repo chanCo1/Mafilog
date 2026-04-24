5f7e6025c64e196788184dbae3d2d256b862f703
chanCo <loinsective@naver.com>
2026-04-25 01:54:37 +0900

    ADD: 도시 검색, 선택 인터페이스

---
+++ src/features/myTravel/interfaces/index.ts
@@ -0,0 +1,38 @@
+/** google places 검색 */
+export interface IGetGooglePlaces {
+  places: {
+    addressComponents: {
+      languageCode: string;
+      longText: string;
+      shortText: string;
+      types: string[];
+    }[];
+    displayName: {
+      languageCode: string;
+      text: string;
+    };
+    formattedAddress: string;
+    id: string;
+    location: ILocation;
+    types: string[];
+  }[];
+}
+
+/** 도시 정보 */
+export interface ICityList {
+  id: string;
+  name: string;
+  location: ILocation;
+  address: string;
+  country: {
+    code: string;
+    name: string;
+  } | null;
+}
+
+interface ILocation {
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+}
