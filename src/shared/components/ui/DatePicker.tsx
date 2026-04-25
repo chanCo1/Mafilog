df5988e3123062ad68d80bbedd1be6577fbacd69
chanCo <loinsective@naver.com>
2026-04-26 00:22:29 +0900

    ADD: 날짜 선택, 값 초기화 추가

--- src/shared/components/ui/DatePicker.tsx
+++ src/shared/components/ui/DatePicker.tsx
@@ -5,27 +5,52 @@
  * @description: DatePicker 컴포넌트, 달력 컴포넌트
  */
 
-import { DayPicker, DateRange, getDefaultClassNames } from 'react-day-picker';
+import { DayPicker, DateRange } from 'react-day-picker';
 import 'react-day-picker/style.css';
-import '@/shared/styles/datePicker.css'
+import '@/shared/styles/datePicker.css';
 import { ko } from 'date-fns/locale';
+import { useState } from 'react';
+import { CircleX } from 'lucide-react';
 
 interface IDatePicker {
-  value: DateRange;
-  onSelected: () => void;
+  selected: DateRange | undefined;
+  onSelected: (value: DateRange | undefined) => void;
 }
 
-export default function DatePicker({ onSelected, value }: IDatePicker) {
-  console.log(getDefaultClassNames())
+export default function DatePicker({ selected, onSelected }: IDatePicker) {
+  const [selectedDate, setSelectedDate] = useState<DateRange | undefined>(
+    selected,
+  );
+
+  /** 날짜 선택 */
+  const onSelectedDate = (value: DateRange | undefined) => {
+    setSelectedDate(value);
+    onSelected(value);
+  };
+
+  /** 선택된 값 초기화 */
+  const dataReset = () => {
+    setSelectedDate(undefined);
+    onSelected(undefined);
+  };
 
   return (
-    <DayPicker
-      showOutsideDays
-      animate
-      mode="range"
-      selected={value}
-      onSelect={onSelected}
-      locale={ko}
-    />
+    <div className="">
+      <div
+        className="text-text-primary flex cursor-pointer items-center justify-end gap-1"
+        onClick={dataReset}
+      >
+        <CircleX className="h-5 w-5" />
+        다시 선택
+      </div>
+      <DayPicker
+        showOutsideDays
+        animate
+        mode="range"
+        selected={selectedDate}
+        onSelect={onSelectedDate}
+        locale={ko}
+      />
+    </div>
   );
 }
