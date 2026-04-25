ce6ccf144ded7272c903f206ca74cab931865b83
chanCo <loinsective@naver.com>
2026-04-25 12:55:24 +0900

    ADD: createNewTravelStepList 분리

---
+++ src/features/myTravel/lib/index.ts
@@ -0,0 +1,5 @@
+export const createNewTravelStepList = [
+  { id: 1, label: '여행지 선택', isComplete: false },
+  { id: 2, label: '날짜 선택', isComplete: false },
+  { id: 3, label: '여행 정보', isComplete: false },
+];
