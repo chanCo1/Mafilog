1ea71ae772f45536e7cc3911e9b5b0bd422a99ba
chanCo <loinsective@naver.com>
2026-04-27 11:12:50 +0900

    ADD: 여행 진행 별 리스트 템플릿 적용

--- src/features/myTravel/pages/MyTravelMainPage.tsx
+++ src/features/myTravel/pages/MyTravelMainPage.tsx
@@ -8,26 +8,42 @@
  */
 
 import { useState } from 'react';
+import { cn } from '@/shared/lib/utils';
 import PageHeader from '@/shared/components/ui/PageHeader';
 import { Button } from '@/shared/components/ui/Button';
 import CreateNewTravelModal from '@/features/myTravel/components/modal/CreateNewTravelModal';
-
-interface IMyTravelMainPage {}
+import { useMyTravelListStore } from '@/shared/stores/useMyTravelListStrore';
+import TravelListCard from '@/features/myTravel/components/modal/TravelListCard';
+import TravelListTemplate from '@/features/myTravel/components/TravelListTemplate';
 
 export default function MyTravelMainPage() {
+  const { upcomingTravel, lastTravel, progressTravel } = useMyTravelListStore();
+
   const [isOpenModal, setIsOpenModal] = useState(false);
 
   return (
     <>
-      <div className="flex flex-col gap-7">
+      <div className="flex flex-col gap-5">
         <PageHeader
           title="내 여행"
           description="내 모든 여행을 한 곳에서 확인해보세요"
         />
-        <div className="flex flex-col items-center gap-1 pt-20">
-          <p className="text-text-secondary text-center text-lg break-keep">
-            아직 다녀온 여행이 없어요. 첫 번째 여행지는 어디인가요?
-          </p>
+        <div
+          className={cn(
+            'flex flex-col items-center gap-1',
+            !upcomingTravel?.length && 'pt-20',
+          )}
+        >
+          {upcomingTravel?.length ? (
+            <div>
+              지금까지 <span className="text-primary font-bold">N번</span>의
+              여행을 다녀왔어요!
+            </div>
+          ) : (
+            <p className="text-text-secondary text-center text-lg break-keep">
+              아직 다녀온 여행이 없어요. 첫 번째 여행지는 어디인가요?
+            </p>
+          )}
           <Button
             variant="secondary"
             size="lg"
@@ -36,6 +52,15 @@ export default function MyTravelMainPage() {
             새 여행 만들기
           </Button>
         </div>
+        {progressTravel.length ? (
+          <TravelListTemplate title="진행중인" list={progressTravel} />
+        ) : null}
+        {upcomingTravel.length ? (
+          <TravelListTemplate title="다가오는" list={upcomingTravel} />
+        ) : null}
+        {lastTravel.length ? (
+          <TravelListTemplate title="지난" list={lastTravel} />
+        ) : null}
       </div>
       <CreateNewTravelModal
         isOpen={isOpenModal}
