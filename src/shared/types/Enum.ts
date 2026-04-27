0158a2213db958751f6ac69293751ff02fbb8972
chanCo <loinsective@naver.com>
2026-04-28 02:48:53 +0900

    ADD: 여행 상태, 동반자, 스타일, 상세 탭 ENUM

--- src/shared/types/Enum.ts
+++ src/shared/types/Enum.ts
@@ -75,5 +75,44 @@ export const ICON_TYPE = {
   /** 기타 */
   ETC: 'etc',
 };
-export type ICON_TYPE =
-  (typeof ICON_TYPE)[keyof typeof ICON_TYPE];
+export type ICON_TYPE = (typeof ICON_TYPE)[keyof typeof ICON_TYPE];
+
+/** 여행 상태 */
+export const TRAVEL_STATUS = {
+  PROGRESS: 'progress', // 진행중인
+  UPCOMING: 'upcoming', // 다가오는
+  LAST: 'last', // 지난
+};
+export type TRAVEL_STATUS = (typeof TRAVEL_STATUS)[keyof typeof TRAVEL_STATUS];
+
+/** 여행 동반자 */
+export const TRAVEL_COMPANION = {
+  ALONE: 'alone', // 혼자
+  FRIEND: 'friend', // 친구와
+  LOVER: 'lover', // 연인과
+  SPOUSE: 'spouse', // 배우자와
+  CHILD: 'child', // 아이와
+  COLLEAGUES: 'colleagues', // 직장동료와
+  FAMILY: 'family', // 가족과
+  PARENTS: 'parents', // 부모님과
+  ETC: 'etc', // 기타
+};
+export type TRAVEL_COMPANION = (typeof TRAVEL_COMPANION)[keyof typeof TRAVEL_COMPANION];
+
+/** 여행 스타일 */
+export const TRAVEL_STYLE = {
+  HEAL: 'heal', // 힐링/휴식
+  FOOD: 'food', // 먹방
+  ACTIVITY: 'activity', // 액티비티
+  CULTURE: 'culture', // 문화/예술
+  EXPERIENCE: 'experience', // 이색/체험
+  ETC: 'etc', // 기타
+};
+export type TRAVEL_STYLE = (typeof TRAVEL_STYLE)[keyof typeof TRAVEL_STYLE];
+
+/** 여행 상세 탭 */
+export const TRAVEL_TAB = {
+  SCHEDULE: 'schedule', // 일정
+  EXPENSES: 'expenses' // 가계부(지출)
+}
+export type TRAVEL_TAB = (typeof TRAVEL_TAB)[keyof typeof TRAVEL_TAB];
