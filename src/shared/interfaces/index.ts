a2f4dd0055ec324c10a1956f1e2524ebea6da196
chanCo <loinsective@naver.com>
2026-05-02 02:07:15 +0900

    ADD: 일정, 가계부 초기값 init 인터페이스

--- src/shared/interfaces/index.ts
+++ src/shared/interfaces/index.ts
@@ -1,7 +1,13 @@
-/** 기본 key: value */
-import { IPlaceList } from '@/features/myTravel/interfaces';
-import { SCHEDULE_TYPE, TRAVEL_STATUS, TRAVEL_PARTNER, TRAVEL_STYLE } from '@/shared/types/Enum';
+import { IPlaceList, IExpensesList } from '@/features/myTravel/interfaces';
+import {
+  SCHEDULE_TYPE,
+  TRAVEL_STATUS,
+  TRAVEL_PARTNER,
+  TRAVEL_STYLE,
+  EXPENSES_CATEGORY_TYPE,
+} from '@/shared/types/Enum';
 
+/** 기본 key: value */
 export interface ILabelValue<T = string | number> {
   label: string;
   value: T;
@@ -32,18 +38,17 @@ export interface ITravelState {
     title: string;
     from: Date;
     to: Date;
-    status: TRAVEL_STATUS,
+    status: TRAVEL_STATUS;
     travelPeriod: number;
     travelStyles: TRAVEL_STYLE[];
     travelPartner: TRAVEL_PARTNER;
     cities: IPlaceList[];
   };
   schedules: ISchedule[];
-  expenses: {
-    day: number | string;
-  }[];
+  expenses: IExpenses[];
 }
 
+/** 일정 리스트 */
 export interface ISchedule {
   day: number;
   date: Date;
@@ -55,9 +60,29 @@ export interface ISchedule {
   }[];
 }
 
+/** 가계부 리스트 */
+export interface IExpenses {
+  day: number;
+  date: Date;
+  list: {
+    type: EXPENSES_CATEGORY_TYPE; // 지출 타입;
+    expenses: IExpensesList;
+    time?: string;
+    memo?: string;
+  }[];
+  dailyExpenses: number; // 일일 총 지출;
+}
+
+export interface IDateFromTo {
+  from: Date;
+  to: Date;
+}
+
 /** 여행 상세 전역 인터페이스 (actions) */
 export interface ITravelActions {
   setInitTravel: (data: ITravelState['travelInfo']) => void;
+  setInitSchedules: (date: IDateFromTo) => void;
+  setInitExpeneses: (date: IDateFromTo) => void;
   setAddScheduleList: (data: ISchedule) => void;
   setDeleteScheduleList: (id: string | number) => void;
   reset: () => void;
