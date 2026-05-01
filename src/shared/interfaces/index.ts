d5295ce84f63b86d34f0aded917a10d58ef7aca3
chanCo <loinsective@naver.com>
2026-05-01 19:54:51 +0900

    ADD: 여행 상세 전역 인터페이스

--- src/shared/interfaces/index.ts
+++ src/shared/interfaces/index.ts
@@ -1,4 +1,7 @@
 /** 기본 key: value */
+import { IPlaceList } from '@/features/myTravel/interfaces';
+import { SCHEDULE_TYPE, TRAVEL_STATUS, TRAVEL_PARTNER, TRAVEL_STYLE } from '@/shared/types/Enum';
+
 export interface ILabelValue<T = string | number> {
   label: string;
   value: T;
@@ -12,12 +15,56 @@ export interface ICountriesData {
   flagEmoji: string;
   latlng: number[];
   region: string;
-  currency: IcountryCurrency;
+  currency: ICountryCurrency;
 }
 
-export interface IcountryCurrency {
+export interface ICountryCurrency {
   [code: string]: {
     name: string;
     symbol: string;
   };
+}
+
+/** 여행 상세 전역 인터페이스 (state) */
+export interface ITravelState {
+  travelInfo: {
+    id: number;
+    title: string;
+    from: Date;
+    to: Date;
+    status: TRAVEL_STATUS,
+    travelPeriod: number;
+    travelStyles: TRAVEL_STYLE[];
+    travelPartner: TRAVEL_PARTNER;
+    cities: IPlaceList[];
+  };
+  schedules: ISchedule[];
+  expenses: {
+    day: number | string;
+  }[];
+}
+
+export interface ISchedule {
+  day: number;
+  date: Date;
+  list: {
+    type: SCHEDULE_TYPE;
+    place?: IPlaceList;
+    time?: string;
+    memo?: string;
+  }[];
+}
+
+/** 여행 상세 전역 인터페이스 (actions) */
+export interface ITravelActions {
+  setInitTravel: (data: ITravelState['travelInfo']) => void;
+  setAddScheduleList: (data: ISchedule) => void;
+  setDeleteScheduleList: (id: string | number) => void;
+}
+
+/** 여행 상세 전역 인터페이스 (getters) */
+export interface ITravelGetters {
+  getTravelInfo: () => ITravelState['travelInfo'];
+  getTravelSchedules: () => ISchedule[];
+  getTravelExpenses: () => any[];
 }
