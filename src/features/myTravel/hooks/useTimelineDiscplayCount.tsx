e79f4ce5f14948bdf6f047ab1c966ee758e1b018
chanCo <loinsective@naver.com>
2026-05-02 11:15:05 +0900

    ADD: 메모를 제외한 장소 인덱스 값 구하는 hook

---
+++ src/features/myTravel/hooks/useTimelineDiscplayCount.tsx
@@ -0,0 +1,37 @@
+/**
+ * @file: useTimelineDiscplayCount.tsx
+ * @author: chad
+ * @since: 2026.05.02 ~
+ * @description: 메모를 제외한 장소 인덱스 값 구하는 hook
+ */
+
+import { useMemo } from 'react';
+import { IScheduleList } from '@/shared/interfaces';
+import { SCHEDULE_TYPE } from '@/shared/types/Enum';
+
+interface IuseTimelineDiscplayCount {
+  type?: SCHEDULE_TYPE;
+  dailyAllSchedule?: IScheduleList[];
+  currentIndex?: number;
+}
+
+export function useTimelineDiscplayCount({
+  currentIndex,
+  dailyAllSchedule,
+  type,
+}: IuseTimelineDiscplayCount) {
+  const displayCount = useMemo(() => {
+    if (!type || !dailyAllSchedule || currentIndex === undefined) return 0;
+    if (type !== SCHEDULE_TYPE.PLACE) return;
+
+    const sliced = dailyAllSchedule?.slice(0, currentIndex + 1);
+
+    const filltered = sliced.filter((schedule) => {
+      return schedule.type === SCHEDULE_TYPE.PLACE;
+    });
+
+    return filltered.length;
+  }, [type, dailyAllSchedule, currentIndex]);
+
+  return displayCount;
+}
