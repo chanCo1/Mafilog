b481bc506edcff0c3c0a00272b651f57d6dcaa62
chanCo <loinsective@naver.com>
2026-05-03 14:46:53 +0900

    UPD: TRAVEL_DETAIL_MOCK_DATA 변경

--- src/features/myTravel/data/index.ts
+++ src/features/myTravel/data/index.ts
@@ -1,4 +1,9 @@
-import { TRAVEL_TAB, TRAVEL_PARTNER, TRAVEL_STYLE, TRAVEL_STATUS } from '@/shared/types/Enum';
+import {
+  TRAVEL_TAB,
+  TRAVEL_PARTNER,
+  TRAVEL_STYLE,
+  TRAVEL_STATUS,
+} from '@/shared/types/Enum';
 
 export const CITY_MOCK_DATA = {
   places: [
@@ -111,62 +116,26 @@ export const TRAVEL_DETAIL_MOCK_DATA = {
     {
       id: 'ChIJAWZKutdIZTURtdOKmJ3WltE',
       types: ['locality', 'political'],
-      formattedAddress: '대한민국 대전광역시',
-      addressComponents: [
-        {
-          longText: '대전광역시',
-          shortText: '대전광역시',
-          types: ['locality', 'political'],
-          languageCode: 'ko',
-        },
-        {
-          longText: '대전광역시',
-          shortText: '대전광역시',
-          types: ['administrative_area_level_1', 'political'],
-          languageCode: 'ko',
-        },
-        {
-          longText: '대한민국',
-          shortText: 'KR',
-          types: ['country', 'political'],
-          languageCode: 'ko',
-        },
-      ],
-      location: {
-        latitude: 36.3577577,
-        longitude: 127.3867458,
-      },
-      displayName: {
-        text: '대전광역시',
-        languageCode: 'ko',
-      },
+      address: '대한민국 대전광역시',
+      country: { name: '대한민국', code: 'KR' },
+      location: { lat: 36.3577577, lng: 127.3867458 },
+      name: '대전광역시',
     },
     {
       id: 'ChIJ51cu8IcbXWARiRtXIothAS4',
       types: ['administrative_area_level_1', 'political'],
-      formattedAddress: '일본 도쿄도',
-      addressComponents: [
-        {
-          longText: '도쿄도',
-          shortText: '도쿄도',
-          types: ['administrative_area_level_1', 'political'],
-          languageCode: 'ko',
-        },
-        {
-          longText: '일본',
-          shortText: 'JP',
-          types: ['country', 'political'],
-          languageCode: 'ko',
-        },
-      ],
-      location: {
-        latitude: 35.6764225,
-        longitude: 139.650027,
-      },
-      displayName: {
-        text: '도쿄도',
-        languageCode: 'ko',
-      },
+      address: '일본 도쿄도',
+      country: { name: '일본', code: 'JP' },
+      location: { lat: 35.6764225, lng: 139.650027 },
+      name: '도쿄도',
+    },
+    {
+      id: 'ChIJ4eIGNFXmAGAR5y9q5G7BW8U',
+      types: ['locality', 'political'],
+      address: '일본 오사카부 오사카시',
+      country: { name: '일본', code: 'JP' },
+      location: { lat: 34.6937249, lng: 135.5022535 },
+      name: '오사카시',
     },
   ],
   schedule: [
