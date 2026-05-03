d3c9731fc433df6cb7f77e52a921b9a3515151e2
chanCo <loinsective@naver.com>
2026-05-03 22:37:22 +0900

    UPD: 메인 설명 아이콘 변경

--- src/features/home/components/briefTab/IntroduceList.tsx
+++ src/features/home/components/briefTab/IntroduceList.tsx
@@ -5,7 +5,7 @@
  * @description: IntroduceList 컴포넌트, 간략 소개 설명 리스트
  */
 
-import { Check } from 'lucide-react';
+import { Dot } from 'lucide-react';
 
 interface IIntroduceList {
   list : string;
@@ -14,7 +14,7 @@ interface IIntroduceList {
 export const IntroduceList = ({ list }: IIntroduceList) => {
   return (
     <div className="flex items-center gap-1 break-keep">
-      <Check className="text-primary h-5 w-5 shrink-0" strokeWidth={4} />
+      <Dot className="text-primary h-5 w-5 shrink-0" strokeWidth={4} />
       <span>{list}</span>
     </div>
   );
