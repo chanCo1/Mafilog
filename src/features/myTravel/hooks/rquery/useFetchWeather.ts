747b257de51a860a34e6974e39782bb3fd041a7c
chanCo <loinsective@naver.com>
2026-05-04 16:03:27 +0900

    ADD: 날씨 조회 react-query

---
+++ src/features/myTravel/hooks/rquery/useFetchWeather.ts
@@ -0,0 +1,39 @@
+import { useQuery } from '@tanstack/react-query';
+import { convertFormattedDate } from '@/shared/lib/utils';
+import { getWeatherByVisualCrossing } from '@/features/myTravel/services/weatherService';
+
+interface IUseFetchWeather {
+  lat: number | undefined;
+  lng: number | undefined;
+  startDate: Date;
+  endDate: Date;
+}
+
+export const useFetchWeather = ({
+  lat,
+  lng,
+  startDate,
+  endDate,
+}: IUseFetchWeather) => {
+  const _startDate = convertFormattedDate(startDate);
+  const _endDate = convertFormattedDate(endDate);
+
+  const query = useQuery({
+    queryKey: ['weather', lat, lng, _startDate, _endDate],
+    queryFn: async () =>
+      await getWeatherByVisualCrossing({
+        lat,
+        lng,
+        startDate: _startDate,
+        endDate: _endDate,
+      }),
+    enabled: !!lat && !!lng && !!startDate && !!endDate,
+    staleTime: 1000 * 60 * 60 * 12,
+    gcTime: 1000 * 60 * 60 * 24,
+  });
+
+  return {
+    ...query,
+    data: query.data,
+  };
+};
