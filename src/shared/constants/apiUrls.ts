9c40293c965e79dd29a88a93a97f2f01b8a64a35
chanCo <loinsective@naver.com>
2026-05-04 17:47:25 +0900

    DEL: api constant 삭제

--- src/shared/constants/apiUrls.ts
+++
@@ -1,5 +0,0 @@
-/**
- * ExchangeRate-API
- * 환율 정보 가져오는 api
- */
-export const EXCHANGE_RATE_API_URL = `https://v6.exchangerate-api.com/v6/${process.env.NEXT_PUBLIC_EXCHANGE_RATE_API_KEY}/latest/KRW`;
