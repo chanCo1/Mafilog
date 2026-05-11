ac4032ab9463b0eea03e71d3f0ef5e959b894604
chanCo <loinsective@naver.com>
2026-05-11 17:42:57 +0900

    ADD: 지출 하단에 통화별 금액 노출 컴포넌트

---
+++ src/features/myTravel/components/detail/expnese/CurrencySpend.tsx
@@ -0,0 +1,36 @@
+/**
+ * @file: CurrencySpend.tsx
+ * @author: chad
+ * @since: 2026.05.11 ~
+ * @description: 지출 하단에 통화별 금액 노출 컴포넌트
+ */
+
+import { convertComma } from '@/shared/lib/utils';
+
+interface ICurrencySpend {
+  currency: string;
+  spend: number;
+  calcExchangeRate?: number;
+}
+
+export default function CurrencySpend({
+  currency,
+  spend,
+  calcExchangeRate,
+}: ICurrencySpend) {
+  return (
+    <div className="flex flex-col items-end">
+      <div className="flex items-center gap-1">
+        <p className="">{currency}</p>
+        <span className="text-state-error max-mobile:text-md text-lg font-bold">
+          {convertComma(spend)}
+        </span>
+      </div>
+      {calcExchangeRate && currency !== 'KRW' && (
+        <span className="text-text-secondary text-sm font-bold">
+          {convertComma(calcExchangeRate)}원
+        </span>
+      )}
+    </div>
+  );
+}
