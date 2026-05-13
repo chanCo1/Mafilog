579dc61de63c351b831ce0aa858f9091f5899206
chanCo <loinsective@naver.com>
2026-05-14 02:18:30 +0900

    UPD: 정산 모달 화면 재구성

--- src/features/myTravel/components/modal/ExpenseSettleUpModal.tsx
+++ src/features/myTravel/components/modal/ExpenseSettleUpModal.tsx
@@ -10,8 +10,9 @@ import { Button } from '@/shared/components/ui/Button';
 import { useTravelExpenseStore } from '@/shared/stores/useTravelExpenseStore';
 import { useTravelInfoStore } from '@/shared/stores/useTravelInfoStore';
 import { convertComma } from '@/shared/lib/utils';
-import { Card } from '@/shared/components/ui/Card';
-import { User, MoveRight, Dot } from 'lucide-react';
+import { User, MoveRight, MessageCircleWarning } from 'lucide-react';
+import MyExpenseWrap from '@/features/myTravel/components/modal/ExpenseSettlement/MyExpenseWrap';
+import Separator from '@/shared/components/ui/Separator';
 
 interface IExpenseSettleUpModal {
   isOpen: boolean;
@@ -59,102 +60,91 @@ export default function ExpenseSettleUpModal({
 
         <div className="scrollbar-hide flex flex-col gap-4 overflow-auto">
           <div className="flex flex-col gap-2">
-            <p className="font-bold">멤버별 지출</p>
-            <Card className="flex flex-col gap-2">
-              {travelInfo.member.map((member) => (
-                <div
-                  key={member.id}
-                  className="flex items-start justify-between"
-                >
-                  <div className="flex items-center gap-1">
-                    <User size={20} className="text-primary" />
-                    <span className="font-bold">{member.name}</span>
-                  </div>
-                  <div className="flex flex-col items-end">
-                    <span className="text-state-error text-lg font-bold">
-                      {convertComma(getTotalSpendAmountByMember(member.id))}원
+            <p className="font-bold">정산</p>
+            {travelInfo.member.length > 1 && getFinalSettlement().length ? (
+              <>
+                {getFinalSettlement().map((settlement) => (
+                  <div
+                    key={`${settlement.sendId}-${settlement.receiveId}`}
+                    className="text-text-secondary flex flex-wrap items-center"
+                  >
+                    <MessageCircleWarning size={18} className="mr-1" />
+                    <span className="text-text-primary font-bold">
+                      {getMemberName(settlement.sendId)}
                     </span>
-                    <span className="text-text-secondary text-sm">
-                      총 결제 금액:{' '}
-                      {convertComma(getTotalPaymentAmountByMember(member.id))}원
+                    &nbsp;
+                    <MoveRight size={20} />
+                    &nbsp;
+                    <span className="text-text-primary font-bold">
+                      {getMemberName(settlement.receiveId)}
                     </span>
+                    &nbsp;
+                    <span className="text-state-error font-bold">
+                      {convertComma(settlement.amount)}원
+                    </span>
+                    을 보내야해요
                   </div>
-                </div>
-              ))}
-            </Card>
+                ))}
+              </>
+            ) : (
+              <div className="flex justify-center">
+                <span className="text-text-secondary text-sm">
+                  정산할 내역이 없어요
+                </span>
+              </div>
+            )}
           </div>
 
+          <Separator position="horizontal" />
+
           <div className="flex flex-col gap-2">
-            <p className="font-bold">정산</p>
-            <Card className="flex flex-col gap-3">
-              {travelInfo.member.length && getAllTotalSpend() ? (
-                <>
-                  {getFinalSettlement().map((settlement) => (
-                    <div
-                      key={`${settlement.sendId}-${settlement.receiveId}`}
-                      className="text-text-secondary flex flex-wrap items-center"
-                    >
-                      <span className="text-text-primary font-bold">
-                        {getMemberName(settlement.sendId)}
-                      </span>
-                      &nbsp;
-                      <MoveRight size={20} />
-                      &nbsp;
-                      <span className="text-text-primary font-bold">
-                        {getMemberName(settlement.receiveId)}
-                      </span>
-                      &nbsp;
-                      <span className="text-state-error font-bold">
-                        {convertComma(settlement.amount)}원
-                      </span>
-                      을 보내야해요
-                    </div>
-                  ))}
-                </>
-              ) : (
-                <div className="flex justify-center">
+            <p className="font-bold">멤버별 지출</p>
+            {travelInfo.member.map((member) => (
+              <div key={member.id} className="flex items-start justify-between">
+                <div className="flex items-center gap-1">
+                  <User size={20} className="text-primary" />
+                  <span className="font-bold">{member.name}</span>
+                </div>
+                <div className="flex flex-col items-end">
+                  <span className="text-state-error text-lg font-bold">
+                    {convertComma(getTotalSpendAmountByMember(member.id))}원
+                  </span>
                   <span className="text-text-secondary text-sm">
-                    정산할 내역이 없어요
+                    총 결제 금액:{' '}
+                    {convertComma(getTotalPaymentAmountByMember(member.id))}원
                   </span>
                 </div>
-              )}
-            </Card>
+              </div>
+            ))}
           </div>
+
+          <Separator position="horizontal" />
 
           <div className="flex flex-col gap-2">
             <p className="font-bold">내 지출 정리</p>
-            <Card className="flex flex-col gap-2">
-              <div className="flex items-baseline gap-1">
-                <span>총 결제 금액:</span>
-                <span className="text-state-error text-lg font-bold">
-                  {convertComma(
-                    getTotalPaymentAmountByMember(travelInfo.member[0]?.id),
-                  )}
-                  원
-                </span>
-              </div>
-              <div className="flex items-baseline gap-1">
-                <span>총 지출 금액:</span>
-                <span className="text-state-error text-lg font-bold">
-                  {convertComma(
-                    getTotalSpendAmountByMember(travelInfo.member[0]?.id),
-                  )}
-                  원
-                </span>
-              </div>
-              <div className="flex items-baseline gap-1">
-                <span>받을 금액:</span>
-                <span className="text-state-success text-lg font-bold">
-                  {convertComma(getMyReceiveAmount(travelInfo.member[0]?.id))}원
-                </span>
-              </div>
-              <div className="flex items-baseline gap-1">
-                <span>내 순수 지출:</span>
-                <span className="text-state-error text-lg font-bold">
-                  {convertComma(getMyNetExpense(travelInfo.member[0]?.id))}원
-                </span>
-              </div>
-            </Card>
+            <MyExpenseWrap
+              name="총 결제 금액"
+              amount={convertComma(
+                getTotalPaymentAmountByMember(travelInfo.member[0]?.id),
+              )}
+            />
+            <MyExpenseWrap
+              name="총 지출 금액"
+              amount={convertComma(
+                getTotalSpendAmountByMember(travelInfo.member[0]?.id),
+              )}
+            />
+            <MyExpenseWrap
+              name="받을 금액"
+              amount={convertComma(
+                getMyReceiveAmount(travelInfo.member[0]?.id),
+              )}
+              className="text-state-success"
+            />
+            <MyExpenseWrap
+              name="내 순수 지출"
+              amount={convertComma(getMyNetExpense(travelInfo.member[0]?.id))}
+            />
           </div>
         </div>
       </div>
