57bb1e19482a5c802c0744437eecf63c864b63f2
chanCo <loinsective@naver.com>
2026-05-14 02:09:58 +0900

    ADD: 내 지출 정리 금액 노출 컴포넌트

---
+++ src/features/myTravel/components/modal/ExpenseSettlement/MyExpenseWrap.tsx
@@ -0,0 +1,31 @@
+/**
+ * @file: MyExpenseWrap.tsx
+ * @author: chad
+ * @since: 2026.05.13 ~
+ * @description: 내 지출 정리 금액 노출 컴포넌트
+ */
+
+import { cn } from "@/shared/lib/utils";
+import { Dot } from "lucide-react";
+
+interface IMyExpenseWrap {
+  name: string;
+  amount: string | number;
+  className?: string;
+}
+
+export default function MyExpenseWrap({
+  amount,
+  name,
+  className,
+}: IMyExpenseWrap) {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center">
+        <Dot />
+        <span>{name}</span>
+      </div>
+      <span className={cn('text-state-error text-lg font-bold', className)}>{amount}원</span>
+    </div>
+  );
+}
