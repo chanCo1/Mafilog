b775d848c4f306a88c2eb1d35513942ddd98ab33
chanCo <loinsective@naver.com>
2026-05-14 13:35:56 +0900

    UPD: 유저 정보 모달 폴더 이동

--- src/shared/components/modal/UserInfoModal.tsx
+++ src/shared/components/modal/UserInfoModal.tsx
@@ -9,7 +9,7 @@ import { SideModal } from '@/shared/components/ui/SideModal';
 import Separator from '@/shared/components/ui/Separator';
 import { Button } from '@/shared/components/ui/Button';
 import { Card } from '@/shared/components/ui/Card';
-import Link from 'next/link';
+import { MYPAGE_LIST } from '@/shared/constants';
 import { useRouter } from 'next/navigation';
 
 interface IUserInfoModal {
@@ -43,8 +43,8 @@ export default function UserInfoModal({ isOpen, handleClose }: IUserInfoModal) {
         <div className="flex gap-1">
           <div className="h-24 w-24 rounded-full bg-amber-50" />
           <div className="flex flex-col">
-            <span className="text-lg font-bold">여행최고</span>
-            <span className="text-text-secondary">test@test.com</span>
+            <span className="text-lg font-bold">{'여행최고'}</span>
+            <span className="text-text-secondary">{'test@test.com'}</span>
           </div>
         </div>
         <Separator position="horizontal" />
@@ -52,18 +52,14 @@ export default function UserInfoModal({ isOpen, handleClose }: IUserInfoModal) {
           <div className="flex flex-col gap-1">
             <span className="font-bold">마이페이지</span>
             <Card className="text-text-secondary flex flex-col">
-              <p
-                className="hover:bg-gray-2 cursor-pointer rounded-lg p-2"
-                onClick={() => handelLinkPage('/my-page/user-info')}
-              >
-                계정 정보
-              </p>
-              <p
-                onClick={() => handelLinkPage('/my-page/my-timeline')}
-                className="hover:bg-gray-2 cursor-pointer rounded-lg p-2"
-              >
-                내 타임라인
-              </p>
+              {MYPAGE_LIST.map((list) => (
+                <p
+                  className="hover:bg-gray-2 cursor-pointer rounded-lg p-2"
+                  onClick={() => handelLinkPage(list.path)}
+                >
+                  {list.name}
+                </p>
+              ))}
             </Card>
           </div>
         </div>
