289cba1e77a6eca03a09f94a3157585b830aec59
chanCo <loinsective@naver.com>
2026-05-14 23:47:38 +0900

    ADD: 타임라인 통계 모달 (모바일 전용)

---
+++ src/features/myPage/components/timeline/modal/TimelineStatisticModal.tsx
@@ -0,0 +1,39 @@
+/**
+ * @file: TimelineStatisticModal.tsx
+ * @author: chad
+ * @since: 2026.05.14 ~
+ * @description: 타임라인 통계 모달 (모바일 전용)
+ */
+
+import { useState } from 'react';
+import { cn } from '@/shared/lib/utils';
+import { SideModal } from '@/shared/components/ui/SideModal';
+import { Button } from '@/shared/components/ui/Button';
+import TimelineStatistic from '@/features/myPage/components/timeline/TimelineStatistic';
+
+interface ITimelineStatisticModal {
+  isOpen: boolean;
+  handleClose: () => void;
+}
+
+export default function TimelineStatisticModal({
+  isOpen,
+  handleClose,
+}: ITimelineStatisticModal) {
+  return (
+    <SideModal
+      isOpen={isOpen}
+      title="통계"
+      handleClose={handleClose}
+      footer={
+        <Button variant="gray" onClick={handleClose}>
+          닫기
+        </Button>
+      }
+    >
+      <div className="scrollbar-hide flex h-full flex-col overflow-auto">
+        <TimelineStatistic />
+      </div>
+    </SideModal>
+  );
+}
