a6a37fbd1b68557c08f07a38ef0272ec165c3551
chanCo <loinsective@naver.com>
2026-05-16 23:17:05 +0900

    UPD: 프리즈마 저장 시 +9시간 적용

--- src/shared/lib/prisma.ts
+++ src/shared/lib/prisma.ts
@@ -6,11 +6,38 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
+const basePrisma = new PrismaClient({ adapter });
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
+  basePrisma.$extends({
+    query: {
+      $allModels: {
+        async create({ args, query }) {
+          const now = new Date();
+          const kstTime = new Date(now.getTime() + 9 * 60 * 60 * 1000);
+
+          // +9 시간으로 저장
+          args.data = {
+            ...args.data,
+            createdAt: kstTime,
+          };
+
+          return query(args);
+        },
+
+        async update({ args, query }) {
+          const now = new Date();
+          const kstTime = new Date(now.getTime() + 9 * 60 * 60 * 1000);
+
+          args.data = {
+            ...args.data,
+            updatedAt: kstTime,
+          };
+          return query(args);
+        },
+      },
+    },
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
