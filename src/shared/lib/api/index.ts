390e5d51813a5cef81477f16a300c5fa7b2ac6a6
chanCo <loinsective@naver.com>
2026-05-16 22:49:46 +0900

    UPD: axiosInstance 함수 -> 변수로 변경

--- src/shared/lib/api/index.ts
+++ src/shared/lib/api/index.ts
@@ -1,28 +1,24 @@
 import axios, { AxiosInstance } from 'axios';
 import { setInterceptors } from '@/shared/lib/api/interceptor';
 
-const baseurl = `${process.env.NEXT_PUBLIC_BASE_URL}/api`;
+const baseURL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api`;
 
 /** 인증 불필요 기본 axios 인스턴스 */
-export const axiosInstance = (url?: string): AxiosInstance => {
-  return axios.create({
-    baseURL: `${baseurl}${url}`,
+export const axiosInstance = axios.create({
+    baseURL,
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json',
     },
-  });
-};
+  })
 
 /** Interceptor가 적용된 axios 인스턴스 */
-export const axiosInstanceWithAuth = (url?: string): AxiosInstance => {
-  return setInterceptors(
+export const axiosInstanceWithAuth = setInterceptors(
     axios.create({
-      baseURL: `${baseurl}${url}`,
+      baseURL,
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
       },
     })
   );
-};
