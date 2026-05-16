68cf2ac2bfab411fe75fc8e3e5a7fb86e8938519
chanCo <loinsective@naver.com>
2026-05-16 23:27:46 +0900

    ADD: 회원가입 요청 api service

---
+++ src/features/auth/services/AuthService.ts
@@ -0,0 +1,22 @@
+/**
+ * @file: AuthService.ts
+ * @author: chad
+ * @since: 2026.05.16 ~
+ * @description: 로그인, 회원가입 관련 api service
+ */
+
+import { IRegisterRequest } from '@/features/auth/interfaces/register.interface';
+import { axiosInstance, axiosInstanceWithAuth } from '@/shared/lib/api';
+
+const API_URL = '/auth';
+
+class _AuthService {
+  /** 회원가입 요청 */
+  async postRegister(data: IRegisterRequest) {
+    const response = await axiosInstance.post(`${API_URL}/register`, data);
+    return response.data;
+  }
+}
+
+const AuthService = new _AuthService();
+export default AuthService;
