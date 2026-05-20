853b78f1bcfff7afc0eaed32addeed34f94b2315
chanCo <loinsective@naver.com>
2026-05-21 02:04:50 +0900

    ADD: 체크리스트 (카테고리/아이템) 등록/수정/삭제 service

--- src/features/myTravel/services/Checklist.service.ts
+++ src/features/myTravel/services/Checklist.service.ts
@@ -7,6 +7,7 @@
 
 import { axiosInstanceWithAuth } from '@/shared/lib/api';
 import { IChecklistResponse } from '@/features/myTravel/interfaces/checklist.interface';
+import { IChecklistRequest } from '@/features/myTravel/interfaces/checklist.interface';
 
 const API_URL = '/travels';
 
@@ -15,6 +16,33 @@ class _ChecklistService {
   async getTravelChecklist(travelId: string): Promise<IChecklistResponse[]> {
     const response = await axiosInstanceWithAuth.get(
       `${API_URL}/${travelId}/checklist`,
+    );
+    return response.data.data;
+  }
+
+  /** 체크리스트 (카테고리/아이템) 등록 */
+  async postChecklist(travelId: string, data: IChecklistRequest) {
+    const response = await axiosInstanceWithAuth.post(
+      `${API_URL}/${travelId}/checklist`,
+      data,
+    );
+    return response.data.data;
+  }
+
+  /** 체크리스트 (카테고리/아이템) 수정 */
+  async patchChecklist(travelId: string, data: IChecklistRequest) {
+    const response = await axiosInstanceWithAuth.patch(
+      `${API_URL}/${travelId}/checklist`,
+      data,
+    );
+    return response.data.data;
+  }
+
+  /** 체크리스트 (카테고리/아이템) 삭제 */
+  async deleteChecklist(travelId: string, data: IChecklistRequest) {
+    const response = await axiosInstanceWithAuth.delete(
+      `${API_URL}/${travelId}/checklist`,
+      { data },
     );
     return response.data.data;
   }
