bf471fe5caae1b172504ce7c87cd242f1b74bf80
chanCo <loinsective@naver.com>
2026-05-23 20:55:31 +0900

    UPD: 정산, 통계 관련 계산 useCalcExpense 훅으로 이동

--- src/features/myTravel/hooks/useCalcExpense.ts
+++ src/features/myTravel/hooks/useCalcExpense.ts
@@ -54,7 +54,7 @@ export const useCalcExpense = (expenses: IExpenseResponse[]) => {
     // );
 
     return Math.max(0, roundDecimal(targetDay.dailyExpense));
-  }, []);
+  }, [expenses]);
 
   /** 일정 별 내 지출 */
   const getDailyMySpend = useCallback(
@@ -113,7 +113,7 @@ export const useCalcExpense = (expenses: IExpenseResponse[]) => {
     (day: number) => {
       const targetDay = findTargetDay(expenses, day);
 
-      if (!targetDay) return 0;
+      if (!targetDay) return [];
 
       const mySpendedList = getMySpendList(targetDay.expenseList);
 
@@ -381,6 +381,143 @@ export const useCalcExpense = (expenses: IExpenseResponse[]) => {
   //   }));
   // },
 
+  // = = = = = = = = = = = = = = = = = = = = = = = = = =
+  // 정산. 통계
+  // = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+  /** 멤버별 총 결제 금액 */
+  const getTotalPaymentAmountByMember = useCallback(
+    (id: string) => {
+      const totalAmount = expenses.reduce((acc, day) => {
+        const dayTotal = day.expenseList
+          .filter((item) => item.payer.userId === id)
+          .reduce((sum, item) => sum + item.calcExchangeAmount, 0);
+        return acc + dayTotal;
+      }, 0);
+
+      return roundDecimal(totalAmount);
+    },
+    [expenses],
+  );
+
+  /** 멤버별 총 지출 금액 */
+  const getTotalSpendAmountByMember = useCallback(
+    (id: string) => {
+      const total = expenses.reduce((acc, day) => {
+        return (
+          acc +
+          day.expenseList.reduce((dayAcc, item) => {
+            const mySpend = item.spender.find(
+              (_spender) => _spender.member.userId === id,
+            );
+            return dayAcc + (mySpend ? mySpend.calcExchangeAmount : 0);
+          }, 0)
+        );
+      }, 0);
+
+      return roundDecimal(total);
+    },
+    [expenses],
+  );
+
+  /** 결제자, 지출자 리스트 */
+  const getSettlementList = useMemo(() => {
+    const settlementMap: Record<string, number> = {};
+
+    expenses.forEach((day) => {
+      day.expenseList.forEach((item) => {
+        // 결제자 정보
+        const payerInfo = {
+          label: item.payer.name,
+          value: item.payer.userId,
+        };
+
+        item.spender.forEach((spender) => {
+          // 지출자 정보
+          const spenderInfo = {
+            label: spender.name,
+            value: spender.member.userId,
+          };
+
+          if (payerInfo.value !== spenderInfo.value) {
+            // 결제자와 지출자가 다르면 키 생성
+            const key = `${spenderInfo.value}_${payerInfo.value}`;
+
+            settlementMap[key] =
+              (settlementMap[key] || 0) + spender.calcExchangeAmount;
+          }
+        });
+      });
+    });
+
+    return Object.entries(settlementMap).map(([key, amount]) => {
+      const [fromId, toId] = key.split('_');
+      return {
+        fromId,
+        toId,
+        amount: roundDecimal(amount),
+      };
+    });
+  }, [expenses]);
+
+  /** 최종 정산 내역 */
+  const getFinalSettlement = useMemo(() => {
+    const settlementMap: Record<string, number> = {};
+
+    getSettlementList.forEach((_list) => {
+      const { fromId, toId, amount } = _list;
+
+      // 정렬 후 키 생성
+      const sortedIds = [fromId, toId].sort();
+      const key = `${sortedIds[0]}_${sortedIds[1]}`;
+
+      if (!settlementMap[key]) settlementMap[key] = 0;
+
+      // 정렬된 순서에서 맨앞과 보낼 사람이 같으면 더하고, 다르면 빼기
+      if (fromId === sortedIds[0]) {
+        settlementMap[key] += amount;
+      } else {
+        settlementMap[key] -= amount;
+      }
+    });
+
+    return (
+      Object.entries(settlementMap)
+        .map(([key, finalAmount]) => {
+          const [id1, id2] = key.split('_');
+
+          return {
+            // 결과값이 양수면 id1 -> id2 보냄
+            // 결과값이 음수면 id2 -> id1 보냄
+            sendId: finalAmount > 0 ? id1 : id2,
+            receiveId: finalAmount > 0 ? id2 : id1,
+            amount: Math.abs(roundDecimal(finalAmount)),
+          };
+        })
+        // 0원인 경우는 제외
+        .filter((item) => item.amount > 0)
+    );
+  }, []);
+
+  /** 내가 받을 금액 구하기 */
+  const getMyReceiveAmount = useCallback((id: string) => {
+    const myReceiveAmount = getFinalSettlement
+      .filter((settle) => settle.receiveId === id)
+      .reduce((acc, item) => acc + item.amount, 0);
+
+    return roundDecimal(myReceiveAmount);
+  }, []);
+
+  /** 내 순수 지출 구하기 */
+  const getMyNetExpense = useCallback((id: string) => {
+    const totalPayment = getTotalSpendAmountByMember(id);
+    const receiveAmount = getMyReceiveAmount(id);
+
+    const calcAmount = totalPayment - receiveAmount;
+
+    return Math.max(0, roundDecimal(calcAmount));
+  }, []);
+
   return {
     getDailyAllSpend,
     getDailyMySpend,
@@ -393,5 +530,11 @@ export const useCalcExpense = (expenses: IExpenseResponse[]) => {
     getAllTotalSpend,
     getAllTotalMySpend,
     getAllTotalSpendByCurrency,
+    getTotalPaymentAmountByMember,
+    getTotalSpendAmountByMember,
+    getSettlementList,
+    getFinalSettlement,
+    getMyReceiveAmount,
+    getMyNetExpense,
   };
 };
