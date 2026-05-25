7702a3a47197392ed9806e9be7804605cf1f3108
chanCo <loinsective@naver.com>
2026-05-25 21:14:09 +0900

    ADD: 여행 도시를 국가별로 그룹화 하는 컴포넌트 적용

--- src/features/myPage/components/timeline/TimelineCard.tsx
+++ src/features/myPage/components/timeline/TimelineCard.tsx
@@ -5,42 +5,55 @@
  * @description: 마이페이지 > 타임라인 카드 컴포넌트
  */
 
-import { useState } from 'react';
-import { cn } from '@/shared/lib/utils';
+import { cn, truncateText } from '@/shared/lib/utils';
 import { Card } from '@/shared/components/ui/Card';
 import { Chip } from '@/shared/components/ui/Chip';
-import { convertComma } from '@/shared/lib/utils';
+import {
+  convertTravelDateRange,
+  convertTravelPartner,
+  convertTravelStyle,
+} from '@/shared/lib/utils';
+import { IMyTravelListResponse } from '@/features/myTravel/interfaces/myTravel.interface';
+import TravelCityGroup from '@/features/myTravel/components/main/TravelCityGroup';
 
 interface ITimelineCard {
   className?: string;
   index: number;
   isSelected: boolean;
+  list: IMyTravelListResponse;
 }
 
 export default function TimelineCard({
   className,
   index = 1,
   isSelected,
+  list,
 }: ITimelineCard) {
   return (
     <div className={cn('flex flex-col gap-1', className)}>
-      <div className="flex items-center">
-        <span>YYYY-MM-DD ~ YYYY-MM-DD (DD일)</span>
-      </div>
+      <span className="text-text-secondary">
+        {convertTravelDateRange(list.from, list.to)}
+      </span>
       <Card select className="flex flex-col gap-3" isSelected={isSelected}>
         <div className="flex flex-col gap-2">
           <div className="flex items-baseline gap-1">
             <span className="text-text-secondary font-bold">#{index}</span>
-            <h3 className="text-lg font-bold">{'도쿄 먹방 여행'}</h3>
+            <h3 className="text-lg font-bold">
+              {truncateText(list.title, 25)}
+            </h3>
           </div>
-          <div>도쿄</div>
+          <TravelCityGroup cities={list.cities} />
           <div className="flex gap-1">
             <Chip size="sm" variant="gray">
-              #{'연인과'}
+              #{convertTravelPartner(list.travelPartner)}
             </Chip>
+            {list.travelStyles.map((style) => (
+              <Chip size="sm" variant="gray">
+                #{convertTravelStyle(style)}
+              </Chip>
+            ))}
           </div>
         </div>
-        <span className="font-bold">{convertComma(1560000)}원 지출</span>
       </Card>
     </div>
   );
