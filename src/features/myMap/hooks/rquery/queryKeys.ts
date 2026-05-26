590c26c3333ee2b76e433e6a8f28cac762deaade
chanCo <loinsective@naver.com>
2026-05-26 17:35:30 +0900

    ADD: 추억 생성 리액트쿼리 생성 및 적용

---
+++ src/features/myMap/hooks/rquery/queryKeys.ts
@@ -0,0 +1,4 @@
+export const memoryKeys = {
+  all: ['memory'] as const,
+  // detail: (?: string) => [...memoryKeys.all, ?] as const,
+};
---
+++ src/features/myMap/hooks/rquery/useCreateMemory.ts
@@ -0,0 +1,35 @@
+/**
+ * @file: useCreateMemory.ts
+ * @author: chad
+ * @since: 2026.05.26 ~
+ * @description: 추억 생성 리액트쿼리
+ */
+
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import MemoryService from '@/features/myMap/services/Memory.service';
+import { toast } from 'sonner';
+import { memoryKeys } from '@/features/myMap/hooks/rquery/queryKeys';
+
+export const useCreateMemory = () => {
+  const queryClient = useQueryClient();
+  const queryKey = memoryKeys.all;
+
+  return useMutation({
+    mutationFn: async (formData: FormData) => {
+      return await MemoryService.createMemory(formData);
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey,
+      });
+
+      toast.success('새 추억을 만들었어요');
+    },
+
+    onError: (error: any) => {
+      const errorMessage = error.response?.data.message;
+      toast.error(errorMessage || '추억을 생성하는 중 오류가 발생했습니다.');
+    },
+  });
+};
