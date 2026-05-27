98b791f1d8e63317c3cc41afaf712ff59e20dd2d
chanCo <loinsective@naver.com>
2026-05-28 03:36:52 +0900

    ADD: 추억 상세 내용 모달 데이터 바인딩

--- src/features/myMap/components/modal/FillMemoryDetailModal.tsx
+++ src/features/myMap/components/modal/FillMemoryDetailModal.tsx
@@ -10,17 +10,30 @@ import { cn } from '@/shared/lib/utils';
 import { Button } from '@/shared/components/ui/Button';
 import { SideModal } from '@/shared/components/ui/SideModal';
 import { useDialogStore } from '@/shared/stores/useDialogStore';
+import { useGetMemoryDetail } from '@/features/myMap/hooks/rquery/useGetMemoryDetail';
+import GoogleMap from '@/shared/components/map/GoogleMap';
+import EmblaCarousel from '@/shared/components/ui/EmblaCarousel';
+import { convertTravelDateRange } from '@/shared/lib/utils';
+import { Card } from '@/shared/components/ui/Card';
+import { Chip } from '@/shared/components/ui/Chip';
+import MemoryScheduleDay from '@/features/myMap/components/modal/fillMemory/MemoryScheduleDay';
 
 interface IFillMemoryDetailModal {
   isOpen: boolean;
   handleClose: () => void;
+  selectedMapId: string | undefined;
 }
 
 export default function FillMemoryDetailModal({
   handleClose,
   isOpen,
+  selectedMapId,
 }: IFillMemoryDetailModal) {
+  const [selectedDay, setSelectedDay] = useState(1);
+
   const { openDialog } = useDialogStore();
+  const { data: memoryDetail } = useGetMemoryDetail(selectedMapId ?? '');
+  console.log('memoryDetail >> ', memoryDetail);
 
   /** 닫기 버튼 클릭 */
   const onClickCloseBtn = () => {
@@ -40,10 +53,14 @@ export default function FillMemoryDetailModal({
     });
   };
 
+  const filteredSchedule = memoryDetail?.schedules?.filter(
+    (schedule) => schedule.day === selectedDay,
+  );
+
   return (
     <SideModal
       isOpen={isOpen}
-      title="상세"
+      title={memoryDetail?.title}
       handleClose={onClickCloseBtn}
       footer={
         <div className={cn('flex w-full justify-between gap-1')}>
@@ -64,7 +81,47 @@ export default function FillMemoryDetailModal({
         </div>
       }
     >
-      <div>FillMemoryDetailModal</div>
+      <div className="flex h-full overflow-auto scrollbar-hide flex-col gap-5">
+        {memoryDetail?.imageUrl.length ? (
+          <EmblaCarousel imageUrls={memoryDetail.imageUrl} />
+        ) : null}
+
+        <div className="flex flex-col gap-2">
+          {memoryDetail?.scheduleTitle && (
+            <h5 className="text-lg font-bold">{memoryDetail?.scheduleTitle}</h5>
+          )}
+          <span>
+            {convertTravelDateRange(memoryDetail?.from, memoryDetail?.to)}
+          </span>
+          {memoryDetail?.memo && <Card>{memoryDetail.memo}</Card>}
+        </div>
+
+        <div className="flex flex-col gap-2 h-full">
+          {memoryDetail && (
+            <div className="scrollbar-hide flex gap-1 overflow-x-auto shrink-0">
+              {memoryDetail?.schedules.map((_day, index) => (
+                <Chip
+                  key={`${_day.day}-${index}`}
+                  size="md"
+                  variant={
+                    selectedDay === _day.day ? 'primary' : 'primaryOutline'
+                  }
+                  onClick={() => setSelectedDay(_day.day)}
+                >{`${_day.day}일차`}</Chip>
+              ))}
+            </div>
+          )}
+          <div className="flex flex-col gap-3">
+            {filteredSchedule?.map((schedule, index) => (
+              <MemoryScheduleDay
+                key={`${schedule.day}-${index}`}
+                schedule={schedule}
+                readonly
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </SideModal>
   );
 }
