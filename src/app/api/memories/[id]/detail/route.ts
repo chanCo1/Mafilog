a866529a856b1c7cc7ede310e02b503b5d823b9a
chanCo <loinsective@naver.com>
2026-05-28 18:23:14 +0900

    ADD: [백엔드] 추억 삭제

--- src/app/api/memories/[id]/detail/route.ts
+++ src/app/api/memories/[id]/detail/route.ts
@@ -11,6 +11,7 @@ import {
   successResponse,
   errorResponse,
 } from '@/shared/backend/utils/apiResponse';
+import { deleteCloudinary } from '@/shared/backend/lib/cloudinary';
 
 /** 추억 상세 조회 */
 export async function GET(
@@ -43,6 +44,38 @@ export async function GET(
     return successResponse(memory);
   } catch (error) {
     console.error('@@ 추억 리스트 조회 에러 >>', error);
+    return errorResponse();
+  }
+}
+
+/** 추억 삭제 */
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const authValidate = await authGuard(request);
+  if (!authValidate.isValid) return authValidate.errorResponse;
+
+  const currentUserId = authValidate.session?.user?.id;
+  if (!currentUserId) return errorResponse('잘 못 된 접근입니다.', 403);
+
+  const memoryId = Number(params.id);
+  if (!memoryId) return errorResponse('잘 못 된 접근입니다.', 403);
+
+  try {
+    const deletedMemory = await prisma.memory.delete({
+      where: { id: memoryId, userId: currentUserId },
+    });
+
+    if (deletedMemory.imageUrl && deletedMemory.imageUrl.length > 0) {
+      await Promise.all(
+        deletedMemory.imageUrl.map((url) => deleteCloudinary(url)),
+      );
+    }
+
+    return successResponse();
+  } catch (error) {
+    console.error('@@ 추억 삭제 에러 >>', error);
     return errorResponse();
   }
 }
