92c336c57ccbab08e6cfe9df2f91618e914c274c
chanCo <loinsective@naver.com>
2026-05-31 19:56:26 +0900

    UPD: 추억채우기 스텝3 순서 변경 및 선택 색상 노출

--- src/features/myTravel/components/modal/AddPlaceModal.tsx
+++ src/features/myTravel/components/modal/AddPlaceModal.tsx
@@ -181,7 +181,7 @@ export default function AddPlaceModal({
         </>
       }
     >
-      <div className="flex h-full flex-col gap-2">
+      <div className="flex h-full flex-col gap-5">
         <Selectbox
           label="여행 일정 선택"
           options={travelDayList}
